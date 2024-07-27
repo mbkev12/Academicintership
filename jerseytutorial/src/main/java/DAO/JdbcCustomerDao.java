7eb62a13804ad100330f4591afd5b40cb15817c7
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2024-07-27 23:49:32 +0100

    Update JdbcCustomerDao.java

--- jerseytutorial/src/main/java/DAO/JdbcCustomerDao.java
+++ jerseytutorial/src/main/java/DAO/JdbcCustomerDao.java
@@ -11,8 +11,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import entities.Customer;
-import entities.Onlinebanking;
 import utils.DbUtil;
 /**
  *
